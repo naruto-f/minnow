b931321d355f6648dc8ceee992940b3d4ce4a9cb
naruto-f <476971219@qq.com>
2024-03-27 01:12:51 +0800

    checkpoint2补充提交

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -1,19 +1,28 @@
 #include "wrapping_integers.hh"
+#include <cmath>
 
 using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
   // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+  return zero_point + static_cast<uint32_t>( n % ( 1LL << 32 ) );
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
   // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  uint64_t cur_num = raw_value_ - zero_point.raw_value_;
+  int64_t count = checkpoint / ( 1UL << 32 ); //由于需要处理特殊情况，所以这里将count声明为有符号整形
+
+  if ( cur_num + count * ( 1UL << 32 ) < checkpoint ) {
+    count = ( checkpoint - cur_num - count * ( 1UL << 32 ) <= cur_num + ( count + 1 ) * ( 1UL << 32 ) - checkpoint )
+              ? count
+              : count + 1;
+  } else {
+    count = ( cur_num + count * ( 1UL << 32 ) - checkpoint <= checkpoint - cur_num - ( count - 1 ) * ( 1UL << 32 ) )
+              ? count
+              : count - 1;
+  }
+  return cur_num + std::max<int64_t>( count, 0 ) * ( 1UL << 32 );
 }
